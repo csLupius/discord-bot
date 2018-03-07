3342dac27f947f2e4fda209e180127b50d16eba6
Çağatay Sarp Aksu <sarpcaksu@gmail.com>
2018-03-07 12:19:42 +0300

    test

--- Server/Controller/FirebaseController.js
+++ Server/Controller/FirebaseController.js
@@ -1,7 +1,7 @@
 var admin = require("firebase-admin");
 
-//var serviceAccount = require("path/to/serviceAccountKey.json");
-var c = {
+var c = require("../key.json");
+/*var c = {
   "type": "service_account",
   "project_id": "skullsofrainbow-skullbot",
   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
@@ -12,20 +12,20 @@ var c = {
   "token_uri": "https://accounts.google.com/o/oauth2/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-wqfdw%40skullsofrainbow-skullbot.iam.gserviceaccount.com"
-}
-
+}*/
+console.log(process.env.FIREBASE_CONFIG);
 var config = {
-  credential : admin.credential.cert(c),
+  credential : admin.credential.cert(),
   databaseURL : "https://skullsofrainbow-skullbot.firebaseio.com"
 }
 
 
-admin.initializeApp(config);
+//admin.initializeApp(config);
 
 
 module.exports = {
   TEST: function () {
-    admin.database().ref('TEST/' + 1).set({
+    admin.database().ref('TEST/' + 22).set({
       test: "TEST COMPLETE"
     });
   }
