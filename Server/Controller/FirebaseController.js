2e63a62855e03c6f69870527740c10f17e30614f
Çağatay Sarp Aksu <sarpcaksu@gmail.com>
2018-03-07 11:41:12 +0300

    work now ?

--- Server/Controller/FirebaseController.js
+++ Server/Controller/FirebaseController.js
@@ -9,7 +9,13 @@ var config = {
   "private_key_id": "88d06199fe919de73d3f7c4cf0561cb8549ee40b",
   "client_email": "firebase-adminsdk-wqfdw@skullsofrainbow-skullbot.iam.gserviceaccount.com",
   "client_id": "105747545999842209605",
+  apiKey : process.env.FIREBASE_APIKEY,
   "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  authDomain: "skullsofrainbow-skullbot.firebaseapp.com",
+  databaseURL: "https://skullsofrainbow-skullbot.firebaseio.com",
+  projectId: "skullsofrainbow-skullbot",
+  storageBucket: "skullsofrainbow-skullbot.appspot.com",
+  messagingSenderId: "18182661522",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://accounts.google.com/o/oauth2/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
@@ -22,7 +28,7 @@ admin.initializeApp(config);
 
 module.exports = {
   TEST : function(){
-      firebase.database().ref('TEST/' + 1).set({
+      admin.database().ref('TEST/' + 1).set({
         test: "TEST COMPLETE"
       });
   }
