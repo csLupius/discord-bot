295518e6c68b0ea786b078196d4f9cf1b769c962
Çağatay Sarp Aksu <sarpcaksu@gmail.com>
2018-03-13 15:36:09 +0300

    log

--- Server/Controller/DatabaseController.js
+++ Server/Controller/DatabaseController.js
@@ -1,5 +1,5 @@
 const { Client } = require("pg");
-console.log(process.env.DATABASE_URL);
+//console.log(process.env.DATABASE_URL);
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true
@@ -9,14 +9,15 @@ var e = {
   TESTcheckIfUserExists : function(discordID){
     var _result = false;
     client.connect();
-    var res = client.query('select id from public."Users" where discord_snowflake =\''+ discordID + '\'');
-    
-    res.on('row', function(row, result){
-      result.addRow(row);
-    });
-    res.on('end', function(result){
-      client.end();
-      _result = result;
+    var res = client.query('select id from public."Users"',[discordID],function(err, res, fields){
+      if(err) throw err;
+
+      console.log("--- Query Fields ------")
+      console.log(fields);
+      console.log("--- Query Res ------")
+      console.log(res);
+
+      _result = res;
     });
     return _result;
   }
